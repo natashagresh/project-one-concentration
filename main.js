bc71cbe3045d5b9690ebabb4bec84145b81e8792
Natasha Gresh <natashagresh@hotmail.com>
2015-11-05 19:15:01 -0500

    starting javascript

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log('Im connected. Yey!');
+
+
+
+// var gameBoard=document.querySelector('game-board');
+var $gameBoard=$('#game-board')
+// var resetButton=document.querySelector('button');
+var $resetButton=$('#button');
+
+var squareOne=document.getElementbyId('square-one');
+var squareTwo=document.getElementbyId('square-two');
+
+///im thinking of doing an array and putting it in a function
+///and then using splice to try and recognise two of the same arrays///
+
+var squares=['squareOne','squareTwo'];
+
+// var squaresToWork=function(){
+
+// }
+
+// $ button.on();
+// button.on ('click'fucntion {
+// 	console.log('you clicked me!');
+// })
+
+
